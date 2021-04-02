1774aabf2b0fffbdaf281a3c3d70af5dfca63f10
BHAVESH GOYAL <bhavstar99@gmail.com>
2021-04-02 10:35:15 +0530

    host changed

--- restaurnt/settings.py
+++ restaurnt/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = ')e24ku@%@nz6n&#y!($-6o@j-2r*n(gxn3k0wp13)urlxf281)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://restaurnat.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['restaurnat.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
